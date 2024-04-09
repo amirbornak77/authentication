7dc45c9d5e8e58e41141270dd923fd46852e8691
amirbornak77 <131398939+amirbornak77@users.noreply.github.com>
2024-04-09 10:19:27 -0700

    Update forms.py

--- account/forms.py
+++ account/forms.py
@@ -1,12 +1,12 @@
 from django import forms
-from .models import CustomUser
+from .models import User
 
 class CustomUserCreationForm(forms.ModelForm):
     password1 = forms.CharField(label='Password', widget=forms.PasswordInput)
     password2 = forms.CharField(label='Password confirmation', widget=forms.PasswordInput)
 
     class Meta:
-        model = CustomUser
+        model = User
         fields = ('email', 'name')
 
     def clean_password2(self):
